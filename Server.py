01eb49eb6c9198ff4aad5cd14e1f5598e897cf87
Bohdan <b.borkivskyy@ucu.edu.ua>
2017-06-22 19:16:32 +0300

    1

--- Server.py
+++ Server.py
@@ -1,2 +1,19 @@
 from PodSixNet.Server import Server
 from PodSixNet.Channel import Channel
+
+
+class ServerChannel(Channel):
+    def __init__(self, *args, **kwargs):
+        Channel.__init__(self, *args, **kwargs)
+        self.id = str(self._server.NextId())
+        intid = int(self.id)
+
+
+class GameServer(Server):
+    channelClass = ServerChannel
+
+    def __init__(self, *args, **kwargs):
+        self.id = 0
+        Server.__init__(self, *args, **kwargs)
+        self.players = {}
+        print('Server started')
