59d5ed10e57e1892593806727d66037e1a159d99
AntonBorkivskyy <borkivskyy@ucu.edu.ua>
2017-06-22 19:15:18 +0300

    Client 0.1

--- Client.py
+++ Client.py
@@ -1,4 +1,28 @@
+import sys
+from time import sleep
+
 from PodSixNet.Connection import connection, ConnectionListener
 
 class Client(ConnectionListener):
-    pass
+    def __init__(self, host, port):
+        self.Connect((host, port))
+        self.players = {}
+
+    def Loop(self):
+        self.Pump()
+        connection.Pump()
+        self.Events()
+
+        if "connecting" in self.statusLabel:
+            self.statusLabel = "connecting..."
+
+
+if len(sys.argv) != 2:
+    print("Usage:", sys.argv[0], "host:port")
+    print("e.g.", sys.argv[0], "localhost:31425")
+else:
+    host, port = sys.argv[1].split(":")
+    c = Client(host, int(port))
+    while 1:
+        c.Loop()
+        sleep(0.001)
