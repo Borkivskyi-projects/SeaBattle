c8a2b3df8b18a47282f113b039e2385418a61f68
AntonBorkivskyy <borkivskyy@ucu.edu.ua>
2017-06-29 21:29:44 +0300

    Player see available games

--- Client.py
+++ Client.py
@@ -8,7 +8,8 @@ class Client(ConnectionListener):
     def __init__(self, host, port, name):
         self.Connect((host, port))
         self.name = name
-        self.players = {}
+        #self.players = {}
+        connection.Send({"action": "nickname", "message": self.name})
 
     def Loop(self):
         self.Pump()
@@ -16,9 +17,15 @@ class Client(ConnectionListener):
 
     def Input(self):
         connection.Send({"action": "message", "message": '1','name':self.name})
+        
+    def Network_show_games(self, data):
+        print("Choose one of the games:\n")
+        print(data['message'])
+        choice = input("Your choice: ")
+        connection.Send({"action": "game_choice", "message": choice})
 
     def Network_message(self, data):
-        print(data['name'] + " : " + data['message'])
+        print(data['name'] + ": " + data['message'])
 
     def Network_connected(self, data):
         self.statusLabel = "connected"
@@ -29,16 +36,17 @@ class Client(ConnectionListener):
         connection.Close()
 
     def Network_disconnected(self, data):
-        self.statusLabel += " - disconnected"
+        self.statusLabel = "disconnected"
 
 
 if len(sys.argv) != 2:
-    print("Usage:", sys.argv[0], "host:port")
-    print("e.g.", sys.argv[0], "localhost:31425")
+    #print("Usage:", sys.argv[0], "host:port")
+    #print("e.g.", sys.argv[0], "localhost:31425")
+    host, port = '', 8000
 else:
     host, port = sys.argv[1].split(":")
     
-    c = Client(host, int(port), input("Enter your username: "))
-    while 1:
-        c.Loop()
-        sleep(0.001)
+c = Client(host, int(port), input("Enter your username: "))
+while 1:
+    c.Loop()
+    sleep(0.001)
